4b9a2fc2074349f4669c9efb618d503b80ffe1b8
Mazhar Hussain Warsi <warsi.mazhar@gmail.com>
2018-02-20 17:46:03 +0530

    Add files via upload

---
+++ scripts/jumpvm-config.ps1
@@ -0,0 +1,7 @@
+﻿$Url = "https://the.earth.li/~sgtatham/putty/latest/w64/putty-64bit-0.70-installer.msi"
+Invoke-WebRequest -Uri "$Url" -OutFile "C:\Packages\putty-64bit-0.70-installer.msi"
+msiexec.exe /i C:\Packages\putty-64bit-0.70-installer.msi  /qn
+
+$Url = "https://winscp.net/download/WinSCP-5.13-Setup.exe"
+Invoke-WebRequest -Uri "$Url" -OutFile "C:\Packages\WinSCP-5.13-Setup.exe"
+msiexec.exe /i C:\Packages\WinSCP-5.13-Setup.exe  /qn
