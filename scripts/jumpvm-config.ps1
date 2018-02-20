55610ac3b9e9dc2c5b6318e69bbdcd5b4189b044
Mazhar Hussain Warsi <warsi.mazhar@gmail.com>
2018-02-20 19:43:09 +0530

    Update jumpvm-config.ps1

--- scripts/jumpvm-config.ps1
+++ scripts/jumpvm-config.ps1
@@ -2,6 +2,6 @@
 Invoke-WebRequest -Uri "$Url" -OutFile "C:\Packages\putty-64bit-0.70-installer.msi"
 msiexec.exe /i C:\Packages\putty-64bit-0.70-installer.msi  /qn | Out-Null
 
-$Url = "https://winscp.net/download/WinSCP-5.13-Setup.exe"
+$Url = "https://spektraazurelabs.blob.core.windows.net/saplabs/WinSCP-5.13-Setup.exe"
 Invoke-WebRequest -Uri "$Url" -OutFile "C:\Packages\WinSCP-5.13-Setup.exe"
 C:\Packages\WinSCP-5.13-Setup.exe  /VERYSILENT | Out-Null
