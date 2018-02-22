8082382109f7f8697affbb70562328029e43302c
Mazhar Hussain Warsi <warsi.mazhar@gmail.com>
2018-02-22 16:45:17 +0530

    Update jumpvm-config.ps1

--- scripts/jumpvm-config.ps1
+++ scripts/jumpvm-config.ps1
@@ -1,7 +1,11 @@
-﻿$Url = "https://the.earth.li/~sgtatham/putty/latest/w64/putty-64bit-0.70-installer.msi"
+$Url = "https://the.earth.li/~sgtatham/putty/latest/w64/putty-64bit-0.70-installer.msi"
 Invoke-WebRequest -Uri "$Url" -OutFile "C:\Packages\putty-64bit-0.70-installer.msi"
 msiexec.exe /i C:\Packages\putty-64bit-0.70-installer.msi  /qn | Out-Null
 
 $Url = "https://spektraazurelabs.blob.core.windows.net/saplabs/WinSCP-5.13-Setup.exe"
 Invoke-WebRequest -Uri "$Url" -OutFile "C:\Packages\WinSCP-5.13-Setup.exe"
 C:\Packages\WinSCP-5.13-Setup.exe  /VERYSILENT | Out-Null
+
+$URL = "https://spektraazurelabs.blob.core.windows.net/saplabs/SAP.zip"
+Invoke-WebRequest -Uri "$Url" -OutFile "C:\Packages\SAP.zip" | Out-Null
+Expand-Archive -Path C:\Packages\SAP.zip -DestinationPath C:\Packages\ | Out-Null
