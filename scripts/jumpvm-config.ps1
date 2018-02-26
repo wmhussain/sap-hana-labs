89a100caeeb2d5b4e4a986cc7314265fbd93e4c8
PraveenAnil <praveendosth@gmail.com>
2018-02-26 20:22:38 +0530

    Update jumpvm-config.ps1

--- scripts/jumpvm-config.ps1
+++ scripts/jumpvm-config.ps1
@@ -6,6 +6,6 @@ $Url = "https://spektraazurelabs.blob.core.windows.net/saplabs/WinSCP-5.13-Setup
 Invoke-WebRequest -Uri "$Url" -OutFile "C:\Packages\WinSCP-5.13-Setup.exe"
 C:\Packages\WinSCP-5.13-Setup.exe  /VERYSILENT | Out-Null
 
-$URL = $Args[1]
+$URL = $Args[0]
 Invoke-WebRequest -Uri "$URL" -OutFile "C:\Packages\HDB_STUDIO_WINDOWS_X86_64.zip" | Out-Null
 Expand-Archive -Path C:\Packages\HDB_STUDIO_WINDOWS_X86_64.zip -DestinationPath C:\Packages\ | Out-Null
